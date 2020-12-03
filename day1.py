9ce2b4f8da394dad2269d77a837bfa38e85eb1c7
Sam Basak <samjbasak@gmail.com>
2020-12-03 19:18:28 +0000

    Tidy data import

--- day1.py
+++ day1.py
@@ -1,17 +1,18 @@
 from itertools import combinations
 import math
 
-with open('day1.txt', 'r') as f:
-    input_data = []
-    for item in f:
-        if item != '\n':
-            input_data.append(int(item.replace('\n', '')))
+def read_in_file(file_name):
+    with open(file_name, 'r') as f:
+        input_data = []
+        for item in f:
+            if item != '\n':
+                yield int(item.replace('\n', ''))
 
+input_data = read_in_file('day1.txt')
 
 def add_up_to_2020(no_of_digits, data=input_data):
-    combs = combinations(data, no_of_digits)
-    for i in combs:
+    for i in combinations(data, no_of_digits):
         if sum(i) == 2020:
             return math.prod(list(i))
             
-print(add_up_to_2020(5))
+print(add_up_to_2020(3))
